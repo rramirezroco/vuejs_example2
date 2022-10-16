abad56f7fc2f4c74377d6f210be9176ddecc96a1
Rosa Ramirez Roco <45204121+rramirezroco@users.noreply.github.com>
2022-10-16 13:20:48 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h2>Películas</h2>
+  <Form msg="Hello Vue 3 in CodeSandbox!" />
+</template>
+
+<script>
+import Form from "./components/Form.vue";
+export default {
+  name: "App",
+  components: {
+    Form: Form,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "./styles/_variables.sccs";
+
+html,
+body {
+  background: $bg;
+  color: $color;
+  margin: 0;
+  font-family: sans-serif;
+}
+h2 {
+  border-bottom: 1px solid $color;
+  padding-bottom: 10px;
+}
+</style>
